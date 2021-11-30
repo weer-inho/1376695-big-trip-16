395280cece1415a6faf0d98eb367f70170348909
Maxim Volkov <mx_ruz_net@hotmail.com>
2021-11-30 22:43:54 +0300

    more short dedicated functions

--- src/main.js
+++ src/main.js
@@ -1,14 +1,14 @@
-import {createSiteInfoTemplate} from './view/site-menu-info.js';
-import {createSiteFiltersTemplate} from './view/site-menu-filters.js';
-import {createMainSortTemplate} from './view/main-sort.js';
-import {createTripEventsList} from './view/trip-events-list.js';
-import {createTripEventsListItemEditTemplate} from './view/trip-events-list-item-edit.js';
-import {createTripEventsListItemTemplate} from './view/trip-event-list-item.js';
-import {RenderPosition, renderTemplate} from './render.js';
+import { createSiteFiltersTemplate } from './view/site-menu-filters.js';
+import { createMainSortTemplate } from './view/main-sort.js';
+import { createTripEventsList } from './view/trip-events-list.js';
+import { createTripEventsListItemEditTemplate } from './view/trip-events-list-item-edit.js';
+import { createTripEventsListItemTemplate } from './view/trip-event-list-item.js';
+import { renderTemplate } from './render.js';
 import { createPageHeaderTemplate } from './view/page-header.js';
 import { createNavigationTemplate } from './view/menu-navigation.js';
 import { createTripCostTemplate } from './view/trip-info-cost.js';
 import { createTripInfoMainTemplate } from './view/trip-info-main.js';
+import { createPageMainTemplae } from './view/page-main.js';
 
 const renderTripInfo = (container)=>{
   renderTemplate(container,createTripInfoMainTemplate());
@@ -28,29 +28,27 @@ const renderPageHeader = (body) => {
   renderTripFilter(body.querySelector('.trip-controls__filters'));
 };
 
+const renderTripItems = (container)=>{
+  renderTemplate(container,createTripEventsListItemEditTemplate());
+  Array.from({length:3}).forEach(()=>{
+    renderTemplate(container, createTripEventsListItemTemplate());
+  });
+};
+
+const renderTripMain = (container)=>{
+  renderTemplate(container, createMainSortTemplate());
+  renderTemplate(container,createTripEventsList());
+  renderTripItems(container.querySelector('.trip-events__list'));
+};
+
 const renderPageMain = (body)=>{
-  throw new Error(!!body);
+  renderTemplate(body, createPageMainTemplae());
+  renderTripMain(body.querySelector('.trip-events'));
 };
 
 const renderPage = (body) => {
   renderPageHeader(body);
   renderPageMain(body);
-  const siteMainElement = body.querySelector('.trip-main');
-
-  renderTemplate(siteMainElement, createSiteInfoTemplate(), RenderPosition.AFTERBEGIN);
-
-  const siteMainControls = siteMainElement.querySelector('.trip-main__trip-controls ');
-  renderTemplate(siteMainControls, createSiteFiltersTemplate());
-
-  const pageBodyContainer = body.querySelector('.trip-events');
-  renderTemplate(pageBodyContainer, createMainSortTemplate());
-  renderTemplate(pageBodyContainer, createTripEventsList());
-
-  const tripEventsList = body.querySelector('.trip-events__list');
-  renderTemplate(tripEventsList, createTripEventsListItemEditTemplate());
-  for (let i = 0; i < 3; i++) {
-    renderTemplate(tripEventsList, createTripEventsListItemTemplate());
-  }
 };
 
 renderPage(document.querySelector('.page-body'));
