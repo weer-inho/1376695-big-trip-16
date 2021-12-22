64e46c8d999d00d50ffc4447ce9fc33570f09db8
Руслан Амиров <kzs1845@gmail.com>
2021-12-22 05:04:54 +0300

    added renderTrip

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import MainSort from './view/main-sort.js';
 import TripList from './view/trip-events-list.js';
 import TripEventsEdit from './view/trip-events-list-item-edit.js';
 import TripEventsItem from './view/trip-event-list-item.js';
-import { renderTemplate, renderElement } from './render.js';
+import { render } from './render.js';
 import PageHeader from './view/page-header.js';
 import MainNavigation from './view/menu-navigation.js';
 import TripCost from './view/trip-info-cost.js';
@@ -16,41 +16,47 @@ const TRIP_COUNT = 15;
 const trips = Array.from({length: TRIP_COUNT}, generateTrip);
 const tripCost = trips.reduce((accumulator, trip) => accumulator + trip.price, 0);
 
+const renderTrip = (listElement, trip) => {
+  const tripComponent = new TripEventsItem(trip);
+  const tripEditComponent = new TripEventsEdit(trip);
+
+  render(listElement, tripComponent.element)
+}
+
 const renderTripInfo = (container)=>{
-  renderElement(container, new InfoMain(trips).element)
-  renderElement(container, new TripCost(tripCost).element)
+  render(container, new InfoMain(trips).element)
+  render(container, new TripCost(tripCost).element)
 };
 
 const renderTripNavigation = (container)=>{
-  renderElement(container, new MainNavigation().element);
+  render(container, new MainNavigation().element);
 };
 const renderTripFilter = (container)=>{
-  // renderTemplate(container, createSiteFiltersTemplate());
-  renderElement(container, new SiteFilters().element)
+  render(container, new SiteFilters().element)
 };
 const renderPageHeader = (body) => {
-  renderElement(body, new PageHeader().element)
+  render(body, new PageHeader().element)
   renderTripInfo(body.querySelector('.trip-main__trip-info'));
   renderTripNavigation(body.querySelector('.trip-controls__navigation'));
   renderTripFilter(body.querySelector('.trip-controls__filters'));
 };
 
 const renderTripItems = (container)=>{
-  renderElement(container, new TripEventsEdit(trips[0]).element)
+  renderTrip(container, trips[0])
 
   for (let i = 1; i < TRIP_COUNT; i++) {
-    renderElement(container, new TripEventsItem(trips[i]).element)
+    render(container, new TripEventsItem(trips[i]).element)
   }
 };
 
 const renderTripMain = (container)=>{
-  renderElement(container, new MainSort().element)
-  renderElement(container, new TripList().element);
+  render(container, new MainSort().element)
+  render(container, new TripList().element);
   renderTripItems(container.querySelector('.trip-events__list'));
 };
 
 const renderPageMain = (body)=>{
-  renderElement(body, new PageMain().element)
+  render(body, new PageMain().element)
   renderTripMain(body.querySelector('.trip-events'));
 };
 
