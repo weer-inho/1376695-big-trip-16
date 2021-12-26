fdbd4d73d06bcb3d1e7d03104ffe040314a8e418
Руслан Амиров <kzs1845@gmail.com>
2021-12-27 02:37:32 +0300

    common methods in abstract class

--- src/view/abstract-view.js
+++ src/view/abstract-view.js
@@ -1,7 +1,27 @@
+import {createElement} from '../render.js';
+
 export default class AbstractView {
+  #element = null;
+
   constructor() {
     if (new.target === AbstractView) {
       throw new Error('Can\'t instant AbstractClass, only concrete one');
     }
+  }
+
+  get element() {
+    if (!this.#element) {
+      this.#element = createElement(this.template);
+    }
+
+    return this.#element;
+  }
+
+  get template() {
+    throw new Error('AbstractClass not implemented, create your own');
+  }
+
+  removeElement() {
+    this.#element = null;
   }
 }
