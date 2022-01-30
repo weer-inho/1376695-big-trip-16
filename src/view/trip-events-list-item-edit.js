68a819193c490a330aacb1413d27befb33456d65
Руслан Амиров <kzs1845@gmail.com>
2022-01-30 14:05:05 +0300

    ограничил возможность ввести в инпут чтолибо кроме чисел

--- src/view/trip-events-list-item-edit.js
+++ src/view/trip-events-list-item-edit.js
@@ -146,7 +146,7 @@ const createListItemEditTemplate = (trip) => {
             <span class="visually-hidden">Price</span>
             &euro;
           </label>
-          <input class="event__input  event__input--price" id="event-price-1" type="text" name="event-price" value="${price}">
+          <input class="event__input  event__input--price" id="event-price-1" type="number" name="event-price" value="${price}">
         </div>
     
         <button class="event__save-btn  btn  btn--blue" type="submit">Save</button>
